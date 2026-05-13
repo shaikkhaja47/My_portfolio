da4134c8fec55624426d63fdec463e6592cafa92
khaja <skhaja7536@gmail.com>
2026-05-13 20:22:20 +0530

    updated css added drak light mode

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Portfolio Loaded Successfully 🚀");
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+
+  document.body.dataset.theme =
+  document.body.dataset.theme === "light"
+  ? "dark"
+  : "light";
+
+  themeToggle.textContent =
+  document.body.dataset.theme === "light"
+  ? "☀️"
+  : "🌙";
+});
